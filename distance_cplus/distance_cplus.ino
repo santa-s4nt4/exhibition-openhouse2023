cd191b7e3afe6f2588857b71e99660ca46960775
Santa Naruse <santa@s4nt4-IAMAS.local>
2023-07-21 03:43:50 +0900

    add method for ip address2string

--- distance_cplus/distance_cplus.ino
+++ distance_cplus/distance_cplus.ino
@@ -9,13 +9,27 @@ float i;                                 //OSCのデータ格納用変数
 
 SONIC_IO sensor;
 
+String ipToString(uint32_t ip) {
+  String result = "";
+
+  result += String((ip & 0xFF), 10);
+  result += ".";
+  result += String((ip & 0xFF00) >> 8, 10);
+  result += ".";
+  result += String((ip & 0xFF0000) >> 16, 10);
+  result += ".";
+  result += String((ip & 0xFF000000) >> 24, 10);
+
+  return result;
+}
+
 // ------------------------------------------------------------
 // Setup 関数　Setup function.
 // ------------------------------------------------------------
 void setup() {
-  M5.begin();             // Init M5StickC.
-  M5.Axp.ScreenBreath(8);// 画面の明るさ
-  M5.Lcd.setRotation(3);  // Rotating display.
+  M5.begin();              // Init M5StickC.
+  M5.Axp.ScreenBreath(8);  // 画面の明るさ
+  M5.Lcd.setRotation(3);   // Rotating display.
 
   // シリアルコンソールの開始　Start serial console.
   Serial.begin(115200);
@@ -40,7 +54,7 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   OscWiFi.subscribe(54414, "/test", i);  // 受信したOSCパケットを直接変数にバインド
-  
+
   sensor.begin(32, 33);
   M5.Lcd.setCursor(0, 0, 4);  // Set the cursor at (0,0) and set the font to a 4 point font.
   M5.Lcd.println(WiFi.localIP());
@@ -51,6 +65,14 @@ void loop() {
   M5.update();
   OscWiFi.update();  // 自動的に送受信するために必須
 
+  if (M5.BtnA.wasPressed()) {
+    OscWiFi.send("192.168.0.2", 54415, "/" + ipToString(WiFi.localIP()), 1);
+  }
+
+  if (M5.BtnA.wasReleased()) {
+    OscWiFi.send("192.168.0.2", 54415, "/" + ipToString(WiFi.localIP()), 0);
+  }
+
   static float distance = 0;
   distance = sensor.getDistance();
   if (distance > 20) {
@@ -58,13 +80,5 @@ void loop() {
     M5.Lcd.printf("%.2fmm", distance);
     OscWiFi.send("192.168.0.2", 54415, "/distance2", distance);
   }
-
-  if (M5.BtnA.wasPressed()) {
-    OscWiFi.send("192.168.0.2", 54415, "/check", 1);
-  }
-  if (M5.BtnA.wasReleased()) {
-    OscWiFi.send("192.168.0.2", 54415, "/check", 0);
-  }
-
   delay(100);
 }
